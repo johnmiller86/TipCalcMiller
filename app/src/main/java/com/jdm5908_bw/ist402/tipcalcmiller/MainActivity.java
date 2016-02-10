088ed59da1c2a5324b62997d503c1b0e29fb36b6
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-10 13:02:51 -0500

    Initial commit

--- app/src/main/java/com/jdm5908_bw/ist402/tipcalcmiller/MainActivity.java
+++ app/src/main/java/com/jdm5908_bw/ist402/tipcalcmiller/MainActivity.java
@@ -2,6 +2,8 @@ package com.jdm5908_bw.ist402.tipcalcmiller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calc);
+
+        SeekBar sB = (SeekBar) findViewById(R.id.percentageSeekBar);
+        final TextView perTV = (TextView) findViewById(R.id.percentageTextView);
+        sB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                perTV.setText(String.valueOf(progress) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
